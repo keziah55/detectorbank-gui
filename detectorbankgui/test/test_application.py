961ce91312129bc558db612e096edd302d9c08be
keziah55 <19816994+keziah55@users.noreply.github.com>
2023-02-03 23:12:16 +0000

    tg-51 adding full app test

---
+++ detectorbankgui/test/test_application.py
@@ -0,0 +1,14 @@
+from detectorbankgui.mainwindow import DetectorBankGui
+import pytest
+
+def test_app(qtbot, audiofile):
+    app = DetectorBankGui(audioFile=audiofile, profile="default")
+    qtbot.addWidget(app)
+    app.show()
+    
+    with qtbot.waitSignal(app.analyser.finished):
+        app._doAnalysis()
+        
+    qtbot.wait(3000)
+    
+    assert len(app.resultsplot._plots) == 1
