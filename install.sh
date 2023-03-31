7ed959786be29734f2fb753036562d2c0111a50c
keziah55 <19816994+keziah55@users.noreply.github.com>
2023-03-31 22:14:39 +0100

    make detectorbank install optional in script

--- install.sh
+++ install.sh
@@ -1,29 +1,30 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 GUI_DIR=$(pwd)
 
-## install all dependencies
+if [[ "$1" == "--install-detectorbank" ]]; then
 
-# detectorbank dependencies
-sudo apt install python3-dev python3-numpy swig build-essential autoconf-archive pkg-config libtool libcereal-dev librapidxml-dev libfftw3-dev doxygen python3 graphviz python3-tap
+  # detectorbank dependencies
+  sudo apt install python3-dev python3-numpy swig build-essential autoconf-archive pkg-config libtool libcereal-dev librapidxml-dev libfftw3-dev doxygen python3 graphviz python3-tap
 
+  ## install DetectorBank
+  cd ..
+  git clone https://github.com/keziah55/DetectorBank.git
+  cd DetectorBank
+  autoreconf --install --verbose
+  mkdir -p build
+  cd build
+  ../configure
+  make
+  sudo make install
+
+  ## back to gui
+  cd $GUI_DIR
+fi
+  
 # gui dependencies
 sudo apt install python3-numpy python3-scipy python3-bs4 python3-pyqt5 python3-qtpy python3-pyqtgraph python3-pip
 python3 -m pip install git+https://github.com/keziah55/CustomPyQtObjects.git
-
-## install DetectorBank
-cd ..
-git clone https://github.com/keziah55/DetectorBank.git
-cd DetectorBank
-autoreconf --install --verbose
-mkdir -p build
-cd build
-../configure
-make
-sudo make install
-
-## back to gui
-cd $GUI_DIR
 
 ## copy script and icon, make .desktop
 
@@ -31,9 +32,8 @@ cd $GUI_DIR
 LOCAL_BIN="/home/$USER/.local/bin"
 mkdir -p $LOCAL_BIN
 if [[ :$PATH: != *:"$LOCAL_BIN":* ]]; then
-  export $PATH=$PATH:$LOCAL_BIN
+  export PATH=$PATH:$LOCAL_BIN
 fi
-chmod 755 detectorbank-gui
 cp detectorbank-gui $LOCAL_BIN
 
 # make .desktop file
