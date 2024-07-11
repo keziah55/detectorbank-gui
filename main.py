be39002ef4bdbbda2ebc873f67cb60c3dd9d995b
keziah55 <19816994+keziah55@users.noreply.github.com>
2024-07-11 21:10:38 +0100

    -am

--- main.py
+++ main.py
@@ -4,35 +4,35 @@
 DetectorBank GUI.
 """
 
-try:
-    from detectorbankgui.mainwindow import DetectorBankGui
-except (ImportError, ModuleNotFoundError):
-    try:
-        import os, sys
-        from pathlib import Path
-        if sys.argv[-1] == "stop now please":
-            # we've already tried this once, don't keep looping
-            raise Exception
+# try:
+#     from detectorbankgui.mainwindow import DetectorBankGui
+# except (ImportError, ModuleNotFoundError):
+#     try:
+#         import os, sys
+#         from pathlib import Path
+#         if sys.argv[-1] == "stop now please":
+#             # we've already tried this once, don't keep looping
+#             raise Exception
             
-        # try adding paths
-        ld_lib_path = os.getenv("LD_LIBRARY_PATH", "")
-        if (p:="/usr/local/lib/") not in ld_lib_path:
-            os.environ["LD_LIBRARY_PATH"] = ld_lib_path + f":{p}"
-        python_version = f"python{sys.version_info.major}.{sys.version_info.minor}"
-        local_lib_path = str(Path.home().joinpath(".local", "lib", python_version, "site-packages"))
-        if local_lib_path not in sys.path:
-            sys.path.append(local_lib_path)
+#         # try adding paths
+#         ld_lib_path = os.getenv("LD_LIBRARY_PATH", "")
+#         if (p:="/usr/local/lib/") not in ld_lib_path:
+#             os.environ["LD_LIBRARY_PATH"] = ld_lib_path + f":{p}"
+#         python_version = f"python{sys.version_info.major}.{sys.version_info.minor}"
+#         local_lib_path = str(Path.home().joinpath(".local", "lib", python_version, "site-packages"))
+#         if local_lib_path not in sys.path:
+#             sys.path.append(local_lib_path)
             
-        # try again
-        # append message to argv so we don't loop infinitely
-        argv = sys.argv + ["stop now please"]
-        os.execv(sys.argv[0], argv)
-    except Exception as err:
-        msg = ("Could not import DetectorBankGui\n "
-                "Please check that both detecorbank and detectorbank-gui are installed "
-                "and any environment variables (e.g. PYTHONPATH or LD_LIBRARY_PATH) "
-                "are set appropriately.")
-        raise RuntimeError(msg)
+#         # try again
+#         # append message to argv so we don't loop infinitely
+#         argv = sys.argv + ["stop now please"]
+#         os.execv(sys.argv[0], argv)
+#     except Exception as err:
+#         msg = ("Could not import DetectorBankGui\n "
+#                 "Please check that both detecorbank and detectorbank-gui are installed "
+#                 "and any environment variables (e.g. PYTHONPATH or LD_LIBRARY_PATH) "
+#                 "are set appropriately.")
+#         raise RuntimeError(msg)
         
 import sys
 import os
@@ -40,6 +40,7 @@ from pathlib import Path
 import argparse
 from qtpy.QtWidgets import QApplication, QSplashScreen
 from qtpy.QtGui import QPixmap
+from detectorbankgui.mainwindow import DetectorBankGui
 
 if __name__ == '__main__':
     
@@ -55,10 +56,12 @@ if __name__ == '__main__':
     
     app = QApplication(sys.argv)
     
+    local_share = Path.home().joinpath(".local", "share")
+    app_name = "detectorbank-gui"
     splash_path = None
     possible_paths = [
         Path("images", "splash.png"),
-        Path.home().joinpath(".local", "share", "detectorbank-gui", "splash.png")
+        local_share.joinpath(app_name, "splash.png")
     ]
     for p in possible_paths:
         if p.is_file():
@@ -68,6 +71,12 @@ if __name__ == '__main__':
         pixmap = QPixmap(str(splash_path))
         splash = QSplashScreen(pixmap)
         splash.show()
+    
+    # set desktop file, if it exists
+    # this allows the correct icon to be shown on wayland
+    p = local_share.joinpath("applications", f"{app_name}.desktop")
+    if p.exists():
+        app.setDesktopFileName(str(p))
     
     window = DetectorBankGui(audioFile=args.input, profile=args.profile)
     window.show()
